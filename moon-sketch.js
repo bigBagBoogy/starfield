48ed4ece1f818b1f5b0383088c0a9ffff3278470
maarten <van_den_bergmaarten@hotmail.com>
2023-03-07 10:55:42 +0700

    first commit

---
+++ moon-sketch.js
@@ -0,0 +1,75 @@
+let moonPic
+function preload() {
+    moonPic = loadImage("bitcoinMoon.png")
+}
+// let angle = 0
+let x
+let y
+let snelheidX
+let snelheidY
+let breed
+let hoog
+
+// function chooseRandomXDirection() {
+//     // if moon gets out of screen, we reset it to the middle and this function
+//     //picks a new direction
+//     dx = random(0, 1)
+//     if (dx > 0.5) {
+//         dx = 1
+//     } else {
+//         dx = -1
+//     }
+// }
+
+function setup() {
+    let cnv = createCanvas(windowWidth, windowHeight)
+    cnv.position(0, 0)
+    cnv.style("z-index", "-1")
+    angleMode(DEGREES)
+    x = windowWidth / 2
+    y = windowHeight / 2
+    breed = 1
+    hoog = breed
+    snelheidX = -2
+    snelheidY = 1
+}
+function draw() {
+    background(0)
+    // translate(windowWidth/2, windowHeight/2)
+    // rotate(angle)
+    // image(moonPic,(breed/2)*-1,(hoog/2)*-1,breed,hoog)
+    image(moonPic, x, y, breed, hoog)
+
+    // angle = angle+0.5
+    x = x + snelheidX
+    y = y + snelheidY
+    breed = breed + 0.5
+    hoog = breed
+    if (
+        x >= windowWidth + 200 ||
+        x <= -200 ||
+        y >= windowHeight + 200 ||
+        y <= -200
+    ) {
+        breed = 1
+        x = windowWidth / 2
+        y = windowHeight / 2
+        snelheidX = random(0, 1)
+        if (snelheidX < 0.5) {
+            snelheidX = -2
+        } else {
+            snelheidX = 2
+        }
+        snelheidY = random(0, 1)
+        if (snelheidY < 0.5) {
+            snelheidY = -1
+        } else {
+            snelheidY = 1
+        }
+        console.log("x", snelheidX)
+        console.log("y", snelheidY)
+    }
+}
+
+// click "go live" below in the task-bar to start up a canvas at
+// http://127.0.0.1:5500/
--- sketchSF.js
+++ sketchSF.js
@@ -1,38 +1,30 @@
-var stars = [];
-var speed;
+let stars = [];
+let speed;
 
 function setup() {
-  var cnv = createCanvas(windowWidth, windowHeight);
+  let cnv = createCanvas(windowWidth, windowHeight);
 
-  cnv.position(0,0);
-  cnv.style('z-index', '-1');
-  
-  for (var i = 0; i < 1800; i++) {
+  cnv.position(0, 0);
+  cnv.style("z-index", "-1");
 
+  for (let i = 0; i < 1800; i++) {
     stars[i] = new Star();
-
   }
-
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-
   speed = map(mouseX, 0, width, 5, 10);
 
   background(0);
 
   translate(width / 2, height / 2);
 
-  for (var i = 0; i < stars.length; i++) {
-
+  for (let i = 0; i < stars.length; i++) {
     stars[i].update();
 
     stars[i].show();
-
   }
-
 }
-
